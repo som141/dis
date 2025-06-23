d9923d39b0d2b81ff8b5a881175811933e2e66c2
som141 <163431679+som141@users.noreply.github.com>
2025-06-24 01:02:11 +0900

    Update evm.java

--- src/main/java/sp1/evm.java
+++ src/main/java/sp1/evm.java
@@ -1,7 +1,7 @@
 package sp1;
 
 public class evm {
-    private static String token = "MTM4NTYzMjc3NjQyNTk2NzgwNw.GbV9c5.KCqGaESgvDlLbvQv06tC6pirym8a6t8XuoqZv4";
+    private static String token = "MTM4NTYzMjc3NjQyNTk2NzgwNw.GJqhLO.aFoZFUTdTztpGUXuAmGGZphDqprzfHDoL1HDnw";
     public static String getToken(){
         return token;
     }
